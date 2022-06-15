03acd7ebcdc0abb4928203618914c965a28a3466
hamzadahir <dahirhamza80@gmail.com>
2022-06-15 17:18:30 +0300

    features: create "Welcome" section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,15 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
+      },
+      screens: {
+        '2xl': '1640px',
       },
     },
     extend: {
       colors: {
         primary: '#1F4181',
+        secondary: '#ED1F27',
       },
     },
   },
