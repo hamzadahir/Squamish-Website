3c63e9b57dcd4d751162a8dd597378ffb151e5fd
hamzadahir <dahirhamza80@gmail.com>
2022-06-21 04:28:19 +0300

    feature: animation on about page

--- src/js/main.js
+++ src/js/main.js
@@ -9,22 +9,37 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
-  // animations on scroll
+  // animations on scroll on Home page
   const trusted = document.getElementById('trusted');
   const frictionless = document.getElementById('frictionless');
   const bridging = document.getElementById('bridging');
 
+  // animations on scroll on About Us page
+  const circle = document.getElementById('circle');
+  const ourProducts = document.getElementById('our-products');
+  const exploreInsights = document.getElementById('explore-insights');
+  const directors = document.getElementById('directors');
+
   const scrollTrigger = (selector) => {
-    if (selector.offsetTop < (window.pageYOffset + window.innerHeight)) {
-      selector.classList.add('animation');
-    } else {
-      selector.classList.remove('animation');
+    if (selector) {
+      if (selector.offsetTop < (window.pageYOffset + window.innerHeight)) {
+        selector.classList.add('animation');
+      } else {
+        selector.classList.remove('animation');
+      }
     }
   };
 
   window.addEventListener('scroll', function () {
+    // Home page
     scrollTrigger(trusted);
     scrollTrigger(frictionless);
     scrollTrigger(bridging);
+
+    // About Us page
+    scrollTrigger(circle);
+    scrollTrigger(ourProducts);
+    scrollTrigger(exploreInsights);
+    scrollTrigger(directors);
   });
 });
