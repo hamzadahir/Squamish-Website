c17635b7980509ac3fe9b83d15b1ddad268e2882
hamzadahir <dahirhamza80@gmail.com>
2022-07-05 20:38:09 +0300

    features: header functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  // header
+  const header = document.getElementsByClassName('header')[0];
+
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 50) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
+    }
+  });
+
+  // tabs
   const tabItem = document.getElementsByClassName('tab-item');
   const panelItem = document.getElementsByClassName('panel-item');
 
-  // tabs
   for (let i = 0; i < tabItem.length; i++) {
     tabItem[i].addEventListener('click', function () {
       const parent = this.closest('.tabs');
