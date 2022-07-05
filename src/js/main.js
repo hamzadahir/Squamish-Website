44a37f786d4170ba3697a783d593d3bcaaa96c40
hamzadahir <dahirhamza80@gmail.com>
2022-07-05 20:23:26 +0300

    features: tabs functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log(111);
+  const tabItem = document.getElementsByClassName('tab-item');
+  const panelItem = document.getElementsByClassName('panel-item');
+
+  // tabs
+  for (let i = 0; i < tabItem.length; i++) {
+    tabItem[i].addEventListener('click', function () {
+      const parent = this.closest('.tabs');
+      for (let i = 0; i < parent.querySelectorAll('.tab-item').length; i++) {
+        parent.querySelectorAll('.panel-item')[i].classList.remove('opened');
+        parent.querySelectorAll('.tab-item')[i].classList.remove('active');
+      }
+      panelItem[i].classList.add('opened');
+      this.classList.add('active');
+    });
+  }
 });
